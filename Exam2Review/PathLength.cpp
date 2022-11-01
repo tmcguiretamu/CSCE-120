573765146cff7c1167497b57979df918db43a1e3
Tim McGuire <tmcguire@tamu.edu>
2022-11-01 15:31:38 -0500

    Update PathLength.cpp

--- Exam2Review/PathLength.cpp
+++ Exam2Review/PathLength.cpp
@@ -9,15 +9,18 @@ double pathLength(double** distance, int size, int* path, int length);
 int main()
 {
    int n=5, m=3;
-   double distance[][5] = {   { 0,  30, 10,  70,  10},
+   double distance[][n] = {   { 0,  30, 10,  70,  10},
                               {30,   0, 45, 100,  50},
                               {10,  45,  0,  85,  20},
                               {70, 100, 85,   0, 100},   
                               {10,  50, 20, 100,   0} };  
-   int path[3] = { 0, 1, 2};
-   double** d = new double*[5];
-   for(int i = 0; i < m; i++)
+   int path[m] = { 0, 1, 2};
+
+   // copy non-dynamic 2D array into a dynamic one.
+   double** d = new double*[n];
+   for(int i = 0; i < n; i++)
       d[i] = distance[i];
+   
    cout << pathLength(d, n, path, m) << endl;
 }
 
