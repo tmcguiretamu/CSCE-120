80abc982399f74e43e6304149ca6b1e7aa3f14f1
Tim McGuire <tmcguire@tamu.edu>
2022-12-06 21:07:13 -0600

    Add files via upload

---
+++ SoftwareDevelopment/my_time.cpp
@@ -0,0 +1,6 @@
+// file: my_time.cpp
+
+bool isLeapYear(int year)
+{
+   return year % 4 == 0;
+}
---
+++ SoftwareDevelopment/my_time.h
@@ -0,0 +1,5 @@
+// file: mytime.h
+#ifndef MYTIME_H
+#define MYTIME_H
+bool isLeapYear(int year);
+#endif
